0fceb7a4ca8ce1ad6c631c7b3ffc8312db61f2ad
Saba Tauqeer <sabatauqeer24@gmail.com>
2026-06-25 07:34:42 +0500

    add navbar in login and signup page

--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../components/layout/Navbar"
 import LoginForm from "../components/user/loginForm"
 
 function LoginPage() {
   return (
     <div>
+      <Navbar/>
      <LoginForm/>
     </div>
     
--- client/src/pages/signupPage.jsx
+++ client/src/pages/signupPage.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../components/layout/Navbar.jsx";
 import SignupForm from "../components/user/signupForm.jsx";
 
 function SignupPage() {
   return (
     <div>
+      <Navbar/>
      <SignupForm/>
     </div>
     
